98907a87fc74277ecdda9efb67eb1352ab544ac4
Sydney Cavallaro <sydneycavallaro@MacBook-Pro-3.local>
2021-03-17 11:54:31 -0400

    homework updates

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -1,6 +1,5 @@
 import os
 import csv
-from itertools import count
 
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
@@ -14,18 +13,21 @@ with open(csvpath) as csvfile:
     
     for row in csvreader:
         
-        profit_losses = int(row[1])
-        
-        months = count(row[0])
+        #part 1 total number of months
+        months = (row[0])
+        total_months = count(months)
         print(months)
+    
+        #part 2 the net total of profit/losses
+        profit_losses = int(row[1])
+        total_profit = sum(profit_losses)
+        print(total_profit)
+    
+        #part 3 calculate the changes in profit/losses, find the average of this change
         
-        #def total_profit():
-        #    total_profit = sum()
-        #total_profit(profit_losses)
-        #print(total_months)
+        #part 4 greatest increase in profits
         
-        #total_profit = sum(profit_losses)
-        #print(total_profit)
+        #part 4 greatest decrease in profits 
         
         
     
