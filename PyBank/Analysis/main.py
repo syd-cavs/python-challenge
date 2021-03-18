fd954de575162632c62db0680fce6fd37aae7c60
Sydney Cavallaro <sydneycavallaro@MacBook-Pro-3.local>
2021-03-17 22:32:00 -0400

    pybank homework update

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -8,23 +8,25 @@ print('---------------------------')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
     csv_header = next(csvreader)
     
+    total_months = 0
+    total_profit = 0
+
     for row in csvreader:
         
         #part 1 total number of months
         months = (row[0])
-        total_months = count(months)
-        print(months)
-    
+        if len(months) > 1:
+            total_months = total_months + 1
+        
         #part 2 the net total of profit/losses
-        profit_losses = int(row[1])
-        total_profit = sum(profit_losses)
-        print(total_profit)
+        net_total = int(row[1])
+        if net_total > 1:
+            total_profit = total_profit + sum(net_total)
     
         #part 3 calculate the changes in profit/losses, find the average of this change
-        average_change = total_profit / total_months
+        #average_change = total_profit / total_months
         
         #part 4 greatest increase in profits
         #finding the max
@@ -32,4 +34,5 @@ with open(csvpath) as csvfile:
         #part 4 greatest decrease in profits 
         #finding the min
         
-    
+print(total_months)  
+print(total_profit)
